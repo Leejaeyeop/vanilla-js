13c0ae638f2eda09e2b8716af1cea6e49a4bd4c8
Leejaeyeop <dlwoduq78@naver.com>
2023-07-30 19:43:30 +0900

    babel 설정 추가
    - babel 설정 추가

--- public/index_bundle.js
+++ public/index_bundle.js
@@ -1,1 +1,96 @@
-(()=>{"use strict";!async function(){(async function(){await async function(){await new Promise(((o,n)=>setTimeout((()=>{console.log("setTimeout 종료!"),o()}),3e3))),console.log("bar 종료")}(),console.log("foo 종료!")})(),console.log("init종료!")}()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/async.js":
+/*!*************************!*\
+  !*** ./src/js/async.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   init: () => (/* binding */ init)\n/* harmony export */ });\nasync function bar() {\r\n    await new Promise((resolve, rejected) =>\r\n        setTimeout(() => {\r\n            console.log(\"setTimeout 종료!\");\r\n            resolve();\r\n        }, 3000)\r\n    );\r\n    console.log(\"bar 종료\");\r\n    return;\r\n}\r\n\r\nasync function foo() {\r\n    await bar();\r\n    // bar()\r\n    console.log(\"foo 종료!\");\r\n}\r\n\r\nasync function init() {\r\n    // await foo()\r\n    foo();\r\n    console.log(\"init종료!\");\r\n}\r\n\r\n\r\n\n\n//# sourceURL=webpack://vanilla/./src/js/async.js?");
+
+/***/ }),
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _async__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./async */ \"./src/js/async.js\");\n\r\n\r\n(0,_async__WEBPACK_IMPORTED_MODULE_0__.init)();\r\n\n\n//# sourceURL=webpack://vanilla/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/index.js");
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
-    mode: "production",
+    mode: "development",
+    devtool: "eval", // 빠르게,
+    resolve: {
+        // entry app의 변환할 파일명
+        extensions: [".jsx", ".js"],
+    },
     entry: {
         index: "./src/js/index.js",
     },
@@ -18,8 +25,30 @@ module.exports = {
                     // 이러한 loader를 통과 시켜 실행, 중요 -> 뒤쪽의 loader가 먼저 실행
                     "style-loader",
                     "css-loader",
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                // plugin 설정들의 모음
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        targets: {
+                                            // 예전 브라우저 지원
+                                            browsers: ["> 1% in KR"],
+                                        }, // 한국에서 1% 이상 점유율 가진 브라우저
+                                        debug: true, // 개발용
+                                    },
+                                ],
+                                "@babel/preset-react",
+                            ],
+                            plugins: [],
+                        },
+                    },
                 ],
+                exclude: /node_modules/, // 제외
             },
         ],
     },
+    plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
 };
